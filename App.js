0b3924ede0b28aa347b6c2902c1b1ac9919c517c
Neosoft <apple@apples-MacBook-Pro.local>
2022-10-31 22:13:28 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import MapView from 'react-native-maps';  
+import { Marker } from 'react-native-maps';  
+const App=()=>{
+  return(
+    <View style={styles.MainContainer}>
+       <MapView 
+         style={styles.mapStyle}  
+          showsUserLocation={false}  
+          zoomEnabled={true}  
+          zoomControlEnabled={true}  
+          initialRegion={{  
+            latitude: 28.579660,   
+            longitude: 77.321110,  
+            latitudeDelta: 0.0922,  
+            longitudeDelta: 0.0421,  
+          }}>  
+  
+          <Marker  
+            coordinate={{ latitude: 28.579660, longitude: 77.321110 }}  
+            title={"JavaTpoint"}  
+            description={"Java Training Institute"}  
+          />  
+        </MapView>  
+    </View>
+  )
+};
+export default App;
+const styles = StyleSheet.create({  
+  MainContainer: {  
+    position: 'absolute',  
+    top: 0,  
+    left: 0,  
+    right: 0,  
+    bottom: 0,  
+    alignItems: 'center',  
+    justifyContent: 'flex-end',  
+  },  
+  mapStyle: {  
+    position: 'absolute',  
+    top: 0,  
+    left: 0,  
+    right: 0,  
+    bottom: 0,  
+  },  
+});  
